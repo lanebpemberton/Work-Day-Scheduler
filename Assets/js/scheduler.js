a757fac4df624c5bd59cc86a77af5331adb98629
Lane Pemberton <lanebpemberton@gmail.com>
2021-03-20 15:15:47 -0400

    Added current day script using moment

--- Assets/js/scheduler.js
+++ Assets/js/scheduler.js
@@ -3,46 +3,55 @@ var timeBlocks =
 {
     nineAM:
     {
+        label:"9AM",
         time:"",
         data: ""
     },
     tenAM:
     {
+        label:"10AM",
         time:"",
         data: ""
     },
     elevenAM:
     {
+        label:"11AM",
         time:"",
         data: ""
     },
     twelvePM:
     {
+        label:"12PM",
         time:"",
         data: ""
     },
     onePM:
     {
+        label:"1PM",
         time:"",
         data: ""
     },
     twoPM:
     {
+        label:"2PM",
         time:"",
         data: ""
     },
     threePM:
     {
+        label:"3PM",
         time:"",
         data: ""
     },
     fourPM:
     {
+        label:"4PM",
         time:"",
         data: ""
     },
     fivePM:
     {
+        label:"5PM",
         time:"",
         data: ""
     }
@@ -55,21 +64,25 @@ function createAllTimeBlocks()
     for(var a in timeBlocks)
     {
         //create schedule block and pass in each property as id
-        createTimeBlock(a);
-    }
+        createTimeBlock(a,timeBlocks[a].label);
+    }   
 }
 
 //create schedule block
-function createTimeBlock(id)
+function createTimeBlock(id,timeLabel)
 {
     var container = $(".container");
     var timeBlock = $(`<div id="${id}" class="time-block"></div>`);
     var row = $('<div class="row"></div>');
-    var hour = $('<div class="hour"></div>')
-    var textArea = $('<div class="textarea"></div>');
-    var saveBtn = $('<div class="saveBtn"></div>');
+    var hour = $(`<h3 class="col-md-2 col-sm-12 hour">${timeLabel}</h3>`)
+    var textArea = $('<textarea class="col-md-8 col-sm-12 textarea"></textarea>');
+    //var inputGroup = $('<div class="input-group col-md-8 col-sm-12"></div>');
+    //var input = $('<input type="text" class="form-control" placeholder="Enter schedule">');
+    var saveBtn = $('<button class="col-md-2 col-sm-12 saveBtn"><i class="far fa-save"></i></button>');
+    //inputGroup.append(input);
     row.append(hour);
     row.append(textArea);
+    //row.append(inputGroup);
     row.append(saveBtn);
     timeBlock.append(row);
     container.append(timeBlock);
@@ -80,3 +93,12 @@ function loopScheduleBlocks()
 {
 
 }
+
+function updateCurrentDay()
+{
+    var currentDay = $('#currentDay');
+    currentDay.text(moment().format('dddd, MMMM Do YYYY'));
+}
+
+createAllTimeBlocks();
+updateCurrentDay();
