97e586765911517fc89d7468cc883a50f62d6f4e
Lane Pemberton <lanebpemberton@gmail.com>
2021-03-20 13:54:54 -0400

    Added custom scheduler script and started building structure

---
+++ Assets/js/scheduler.js
@@ -0,0 +1,61 @@
+//create schedule object
+var schedules = 
+{
+    nineAM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    tenAM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    elevenAM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    twelvePM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    onePM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    twoPM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    threePM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    fourPM:
+    {
+        timeSlot:"",
+        data: ""
+    },
+    fivePM:
+    {
+        timeSlot:"",
+        data: ""
+    }
+}
+
+//function that loops schedule object and creates schedule blocks
+function createScheduleBlocks()
+{
+    
+}
+
+//create function that loops through schedule object and set correct color
+function loopScheduleBlocks()
+{
+
+}
