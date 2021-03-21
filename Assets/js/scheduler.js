f1f0d20c9b39e3fd5510b853a4bd1c254e2b790a
Lane Pemberton <lanebpemberton@gmail.com>
2021-03-21 16:13:57 -0400

    Added function to compare time blocks to current time

--- Assets/js/scheduler.js
+++ Assets/js/scheduler.js
@@ -65,31 +65,45 @@ function createAllTimeBlocks()
     {
         //create schedule block and pass in each property as id
         createTimeBlock(a,timeBlocks[a].label);
+        //compare newly created timeblock with current time
+        checkTimeBlock(a,timeBlocks[a].time);
     }   
 }
 
 //create schedule block
 function createTimeBlock(id,timeLabel)
 {
+    //get existing container from premade html
     var container = $(".container");
+    //create dom elements to append to container
     var timeBlock = $(`<div id="${id}" class="time-block"></div>`);
     var row = $('<div class="row"></div>');
     var hour = $(`<h3 class="col-md-2 col-sm-12 hour">${timeLabel}</h3>`)
     var textArea = $(`<textarea id="text_${id}" class="col-md-8 col-sm-12 textarea"></textarea>`);
     var saveBtn = $(`<button id="save_${id}" class="col-md-2 col-sm-12 saveBtn"><i class="far fa-save"></i></button>`);
-    //inputGroup.append(input);
+    //append respective elements
     row.append(hour);
     row.append(textArea);
-    //row.append(inputGroup);
     row.append(saveBtn);
     timeBlock.append(row);
     container.append(timeBlock);
 }
 
-//loop through time object and set correct color
-function loopTimeBlocks()
+//check timeblock against 
+function checkTimeBlock(id,timeBlockTime)
 {
-
+    //get time block text area
+    var textArea = $(`#text_${id}`);
+    //check current time against timeblock's time
+    if(moment(timeBlockTime, 'h:mma').isBefore(currentTime))
+    {
+        //remove present, future and add past class
+        textArea.removeClass('present future').addClass('past');
+    }else
+    {
+        //remove present, past and add future class
+        textArea.removeClass('present past').addClass('future');
+    }
 }
 
 //create global variable that holds current time
@@ -104,4 +118,3 @@ function updateCurrentDay()
 
 updateCurrentDay();
 createAllTimeBlocks();
-loopTimeBlocks();
